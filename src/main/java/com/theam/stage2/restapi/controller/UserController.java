158b6118c0452fa085c744d089465447628821a8
Víctor Ceballos Espinosa <victcebesp@gmail.com>
2018-05-08 18:48:18 +0100

    add and select developed

---
+++ src/main/java/com/theam/stage2/restapi/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.theam.stage2.restapi.controller;
+
+import com.theam.stage2.restapi.model.User;
+import com.theam.stage2.restapi.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping(path = "/add", headers = "Accept=application/json", produces = "application/json", consumes = "application/json")
+    public @ResponseBody User addUser(@RequestBody User user) {
+        userRepository.save(user);
+        return user;
+    }
+
+    @GetMapping(path = "/all", produces = "application/json")
+    public @ResponseBody Iterable<User> getAllCustomers() {
+        return userRepository.findAll();
+    }
+}
