29cec5be5bacb0a6a3b8cf2ea1ab3b38cec4ba80
Víctor Ceballos Espinosa <victcebesp@gmail.com>
2018-05-11 06:30:13 +0100

    Refactor: unused import removed

--- src/main/java/com/theam/stage2/restapi/config/SecurityConfiguration.java
+++ src/main/java/com/theam/stage2/restapi/config/SecurityConfiguration.java
@@ -5,7 +5,6 @@ import com.theam.stage2.restapi.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
