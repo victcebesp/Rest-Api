3fbae9c14f1569406e0510720675715eea9b4b04
Víctor Ceballos Espinosa <victcebesp@gmail.com>
2018-05-16 23:10:11 +0100

    removed_mysql_scheme_bug

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-insert into db_users_and_customers.role (role, role_id) values ("ADMIN", 1);
-insert into db_users_and_customers.role (role, role_id) values ("USER", 2);
-insert into db_users_and_customers.user (user_id, username, password) values (1, "admin", "$2a$11$Rj5m5B9oEsrEUYILVpglyulqwc8CPIwbKzXHgTd9mMP5o6OsDsQPy");
-insert into db_users_and_customers.user_roles (user_user_id, roles_role_id) values (1, 1);
+insert into role (role, role_id) values ("ADMIN", 1);
+insert into role (role, role_id) values ("USER", 2);
+insert into user (user_id, username, password) values (1, "admin", "$2a$11$Rj5m5B9oEsrEUYILVpglyulqwc8CPIwbKzXHgTd9mMP5o6OsDsQPy");
+insert into user_roles (user_user_id, roles_role_id) values (1, 1);
